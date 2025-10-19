1af22febd1a9a969e5951efe151b278ebf7ce105
Patricia <patricianjuguna084@gmail.com>
2025-10-19 21:11:57 +0300

    SCRUM-12 fix:render AppRouter within App.jsx to define the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import AppRouter from "./router/AppRouter"
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">SolarMatch Web App</h1>
+      <AppRouter/>
     </>
   )
 }
