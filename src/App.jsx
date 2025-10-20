fcbf826bd33365974c85605846d2f66262bb75fa
Patricia <patricianjuguna084@gmail.com>
2025-10-20 12:01:21 +0300

    SCRUM-13 feat(ui): integrate toast notifications using Sonner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import AppRouter from "./router/AppRouter"
 import SunnyWidget from "./components/ai/SunnyWidget"
+import { Toaster } from "./components/common/Notification"
 
 function App() {
   return (
     <>
+      <Toaster />
       <h1 className="text-3xl font-bold underline">SolarMatch Web App</h1>
       <AppRouter/>
       <SunnyWidget/>
