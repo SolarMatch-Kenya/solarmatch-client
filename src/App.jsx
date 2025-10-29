8a8628d6f72aa70198223bf8c833a1e488dca8e2
Patricia <patricianjuguna084@gmail.com>
2025-10-29 10:45:47 +0300

    fix unused import in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import AppRouter from "./router/AppRouter"
 import SunnyWidget from "./components/ai/SunnyWidget"
 import { Toaster } from "./components/common/Notification"
-import ExamplePagination from "../examplepagination"
 
 function App() {
   return (
